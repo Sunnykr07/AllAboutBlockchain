cb5705fa8a0e919e49fcf31fac2645b77a99dc84
sunnykr07 <sunnykr8005@gmail.com>
2023-09-29 14:13:20 +0530

    hatadooyaar1234

---
+++ AlphaFolder/globalvariable.sol
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: GPL-3.0
+// There is no concept of none and null in solidity. jo default value hoti hain uss data type ki woh store ho jati hain
+// in that data type if it is not declared prior.
