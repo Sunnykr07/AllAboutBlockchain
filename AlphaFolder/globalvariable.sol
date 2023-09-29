ff0f2ea452332f516032d7f55d5470f2b44348e0
sunnykr07 <sunnykr8005@gmail.com>
2023-09-29 17:21:56 +0530

    hatadooyaar1234

--- AlphaFolder/globalvariable.sol
+++ AlphaFolder/globalvariable.sol
@@ -2,11 +2,9 @@
 // There is no concept of none and null in solidity. jo default value hoti hain uss data type ki woh store ho jati hain
 // in that data type if it is not declared prior.
 
+// SPDX-License-Identifier: GPL-3.0
 
-
-
-
-// SPDX-License-Identifier: GPL-3.0// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.5.0 < 0.9.0;
 contract State
 {
     // Koie Bhi variable hamare contract level par declare hoo woh state varible kahlata hain.
@@ -15,6 +13,26 @@ contract State
     // try karo ki kaam state variable store karo taki kam gas use hoo.
 
 
-    uint age;
     
+    // "public" issi bhi variable k aaage likhne se uss variable ka function ban jata hain.
+    uint public age;
+
+    // THERE ARE THREE METHODS TO INTILIAZE THE STATE VARIABLE :-
+    /* FIRST - PEHLE HI INTILIZE KAR DOO
+       uint public age = 17;
+       SECOND - CONSTRUCTOR CREATE KAR DOO
+       consructor() public
+       {
+           age =17;
+       }    
+       THIRD -  MAKE SETTER FUNCTION
+       function setAge() public
+       {
+           age =10;
+       }    
+
+
+    
+    */
+
 }
