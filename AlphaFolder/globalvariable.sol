6c2edc886910700b13a5573da0f096ffb20fe431
sunnykr07 <sunnykr8005@gmail.com>
2023-09-29 16:46:42 +0530

    hatadooyaar1234

--- AlphaFolder/globalvariable.sol
+++ AlphaFolder/globalvariable.sol
@@ -1,3 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0
+
 // There is no concept of none and null in solidity. jo default value hoti hain uss data type ki woh store ho jati hain
 // in that data type if it is not declared prior.
+
+
+
+
+
+// SPDX-License-Identifier: GPL-3.0// SPDX-License-Identifier: GPL-3.0
+contract State
+{
+    // Koie Bhi variable hamare contract level par declare hoo woh state varible kahlata hain.
+    // jaise niche wal uint age mein age state variable hain.
+    // state variable contract level par permanently store hota hain and uses gases,so
+    // try karo ki kaam state variable store karo taki kam gas use hoo.
+
+
+    uint age;
+    
+}
